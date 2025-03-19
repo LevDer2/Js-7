df5a7896a5906a97cd0a19be1d425965e84f3270
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-03-19 19:55:10 +0200

    tomorrow

---
+++ js/js.js
@@ -0,0 +1,38 @@
+// // Task 1
+// const arr = [1, 4, 5];
+// arr [1] = 10;
+// console.log(arr);
+
+// Task 2
+
+// const ar = ["lev", "vlad", "nazar"];
+// ar [3] = "company";
+// console.log(ar);
+
+// Task 3
+
+// const glist = [6, 4, 3];
+// let sum = 0;
+
+// for (let num of glist){
+//     sum += num;
+// }
+
+// console.log(sum);
+
+// Task 4
+
+// const arrr = [2, 5, 12, 4, 7];
+// for(let i of arrr) {
+//     console.log(i);
+// }
+
+// Task 5
+
+const st = ["Lev", "Nazar", "Antonion", "Ramanovuchi", "Vlad"];
+for (let i = 0; i < st.length; i += 1) {
+    if (st[i].length > 5) {
+        console.log(st[i]);
+        
+    }
+}
